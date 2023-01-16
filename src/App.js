74061386ee81f2df7c1cb29047baba4632b16fbd
terrence <terrence.budnik@gmail.com>
2023-01-16 15:13:37 -0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import Header from "./components/Header";
 import ContactForm from "./components/ContactForm";
+
+import "./css/app.css";
 
 
 function App() {
-  return (
-    <div className="App">
-      <ContactForm />
-    </div>
-  );
-}
+ return (
+  <div className="app">
+    <Header />
+  </div>
+ )
+ }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import "../css/header.css"
+
+const Header = () => {
+    return (
+        <div className="header">Welcome to the Contact Form</div>
+    );
+};
+
+export default Header;
